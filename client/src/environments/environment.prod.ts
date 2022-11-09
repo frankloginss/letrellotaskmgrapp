5bc566d66bd2dcc532be2914325532e54c47cb42
Dev Francis <30405221+frankloginss@users.noreply.github.com>
2022-11-09 03:57:58 -0600

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4001/api',
-  socketUrl: 'http://localhost:4001'
+  apiUrl: 'http://letrello-taskmgrapp.cf:4001/api',
+  socketUrl: 'http://letrello-taskmgrapp.cf:4001'
 };
